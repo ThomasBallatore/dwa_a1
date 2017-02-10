12414c46d2e63a9e4236d7e248121277f0729952
Thomas Ballatore <ballatore@lakebasin.org>
2017-02-10 10:48:23 +0900

    Added link, for testing purposes

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <h1>Just testing!</h1>
+
 <img src='/images/thomas_ballatore.jpg' alt='Yours Truly'>
+
+<a href='https://www.youtube.com/channel/UCEMuOBrjiLqB4IkWIczoYow'>Check out my YouTube channel for some python tutorials</a>
